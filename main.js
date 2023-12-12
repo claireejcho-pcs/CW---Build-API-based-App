b7d6084e85b67d9c3052b69c06a69833a9826549
ajPCS <ajmcguire@pcs-nyc.org>
2023-12-12 13:15:34 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+//let url = 'https://poetrydb.org/random,linecount/1;10/title,author,lines.json'
+let url = 'https://poetrydb.org/author,title/Shakespeare;Sonnet'
+
+
+const button = document.getElementById('button');
+const poemZone = document.getElementById('poem');
+let poemHolder = [];
+
+async function requestPoem(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    const randomIndex = Math.floor(Math.random() * data.length);
+    console.log(randomIndex);
+    const poem = data[randomIndex]["lines"].join(' <br> ');
+
+
+    poemZone.innerHTML = poem;
+    poemHolder = data;
+}
+requestPoem(url);
+
+function newPoem(data) {
+
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const poem = data[randomIndex]["lines"].join(' <br> ');
+    poemZone.innerHTML = poem;
+}
+
+// setInterval(newPoem(poemHolder), 5000);    // how often reloads
+
+
+// button.onclick = () => {
+//     body.innerHTML = requestPoem(url)
+// }
